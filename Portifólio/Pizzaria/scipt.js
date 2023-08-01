dcd2f4e501fa775de8b57abca8e3a3535233fcfe
kaputino99 <kaio.apolo@outlook.com>
2023-08-01 14:43:02 -0300

    Adicionando pages
    Adicionando a pagina de Pizzaria, Lane pages.

---
+++ Portifólio/Pizzaria/scipt.js
@@ -0,0 +1,28 @@
+const botao = document.querySelector('button.abrir')
+const nav = document.querySelector('nav')
+const botaoFechar = document.querySelector('button.fechar')
+
+ botao.addEventListener('click', ()=>{
+    nav.classList.add('mostrar')
+ })
+ botaoFechar.addEventListener('click', ()=>{
+    nav.classList.remove('mostrar')
+ })
+
+ var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
